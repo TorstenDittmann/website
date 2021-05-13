44368e2190a6599d45212c570b96e6086b6364da
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-05-13 19:48:00 +0200

    add error handliong

--- api/dns.js
+++ api/dns.js
@@ -1,9 +1,12 @@
 import dns from "dns";
+
 export default (req, res) => {
     dns.resolveAny("www.appwrite.io", (err, ret) => {
-        if (err) throw err;
+        if (err) {
+            res.status(400).json(err.message);
+            return;
+        }
 
-        res.json(ret)
+        res.json(ret);
     });
-
 }
